17625a2f47fc040c3fe7776bab4c159680205d8d
FreddyPashley <fredpashley1@gmail.com>
2022-05-11 09:34:15 +0100

    Insertion Sort

--- src/utilities.py
+++ src/utilities.py
@@ -7,3 +7,13 @@ def bubbleSort(iterable):
 				iterable[j+1], iterable[j] = iterable[j], iterable[j+1]
 	return iterable
 
+
+def insertionSort(iterable):
+	for i in range(1, len(iterable)):
+		key = iterable[i]
+		j = i-1
+		while j >=0 and key < iterable[j] :
+				iterable[j+1] = iterable[j]
+				j -= 1
+		iterable[j+1] = key
+	return iterable
