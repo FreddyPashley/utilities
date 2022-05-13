949a599b673fe39185c65adc31391fe7c8e6a19c
FreddyPashley <fredpashley1@gmail.com>
2022-05-13 13:10:52 +0100

    1.0 Beta

--- src/utilities.py
+++ src/utilities.py
@@ -1,15 +1,36 @@
-version = "0.7"
+version = "1.0"
 DEBUG = False
 GRAPH = False
 from random import randint
-if GRAPH: import matplotlib.pyplot as plt
+import time
+import matplotlib.pyplot as plt
+import math
 
 
 if DEBUG:
-	def makeTest(length,low,high):
-		return [randint(low,high) for i in range(length)]
+	def makeTest(length,low,high,timer=False):
+		t = Timer()
+		r = [randint(low,high) for i in range(length)]
+		t.stop()
+		if timer or DEBUG: return t.return_result(r)
+		else: return r
 
-def is_sorted(iterable:list):
+class Timer():
+	def __init__(self):
+		self.start()
+
+	def start(self):
+		self.time1 = time.time()
+
+	def stop(self, decimal_places=None):
+		self.time2 = time.time()
+		self.result = self.time2 - self.time1 if decimal_places is None else round(self.time2 - self.time1, decimal_places)
+		return self.result
+
+	def return_result(self, result):
+		return result, self.result
+
+def is_sorted(iterable:list, timer=False):
 	"""
 	Checks if a list is sorted.
 	
@@ -19,13 +40,16 @@ def is_sorted(iterable:list):
 	The object to iterate through.
 	"""
 	sorted = True
+	t = Timer()
 	for i in range(len(iterable)):
-			if i < len(iterable)-1 and iterable[i] > iterable[i+1]:
-			  sorted = False; break
-	return sorted
+		if i < len(iterable)-1 and iterable[i] > iterable[i+1]:
+		  sorted = False; break
+	t.stop()
+	if timer or DEBUG: return t.return_result(sorted)[0]
+	else: return sorted
 	
 	
-def bubbleSort(iterable:list):
+def bubbleSort(iterable:list, timer=False):
 	"""
 	Represents a bubble sort.
 	
@@ -36,16 +60,25 @@ def bubbleSort(iterable:list):
 	iterable: `list`
 	The object to iterate through.
 	"""
+	t = Timer()
 	while not is_sorted(iterable):
 		for j in range(len(iterable) - 1):
 			if iterable[j] > iterable[j+1]:
-				if DEBUG:
-					print("Bubbling "+str(j))
+				if DEBUG: print("Swapping "+str([iterable[j],iterable[j+1]]))
 				iterable[j+1], iterable[j] = iterable[j], iterable[j+1]
+			if GRAPH: #note: i have experimented with using a bar chart, however i found that a line graph looks a lot better
+				plt.plot(iterable)
+				plt.xlabel("Position in array")
+				plt.ylabel("Value of number in position")
+				plt.draw()
+				plt.pause(0.0001)
+				plt.clf()
+	t.stop()
+	if timer or DEBUG: return t.return_result(iterable)
 	return iterable
+
 	
-	
-def insertionSort(iterable:list):
+def insertionSort(iterable:list, timer=False):
 	"""
 	Represents an insertion sort.
 	
@@ -56,7 +89,8 @@ def insertionSort(iterable:list):
 	iterable: `list`
 	The object to iterate through.
 	"""
-	if not is_sorted(iterable):
+	t = Timer()
+	while not is_sorted(iterable):
 		for i in range(1, len(iterable)):
 			if DEBUG: print("Insertion index "+str(i)+" / "+str(len(iterable)))
 			key = iterable[i]
@@ -64,33 +98,29 @@ def insertionSort(iterable:list):
 			while j >=0 and key < iterable[j]:
 				if DEBUG:
 					print("Finding place in sorted half")
-				if GRAPH:
-					plt.plot(iterable)
-					plt.xlabel("Position in array")
-					plt.ylabel("Value of number in position")
-					plt.draw()
-					plt.pause(0.0001)
-					plt.clf()
 				iterable[j+1] = iterable[j]
 				j -= 1
 				iterable[j+1] = key
-	return iterable
+			if GRAPH:
+				plt.plot(iterable)
+				plt.xlabel("Position in array")
+				plt.ylabel("Value of number in position")
+				plt.draw()
+				plt.pause(0.0001)
+				plt.clf()
+	t.stop()
+	if timer or DEBUG: return t.return_result(iterable)
+	else: return iterable
+
 	
-	
-def mergeSort(iterable:list, zero:int, iterable_length_minus_one:int):
+def mergeSort(iterable:list, timer=False):
 	"""
 	Represents a merge sort.
 	
 	# Attributes
 	
 	iterable: `list`
-	The object to iterate through.
-	
-	zero: `int`
-	Always pass 0 into the merge sort.
-	
-	iterable_length_minus_one: `int`
-	Pass the length of the iterable minus 1 into the merge sort.
+	The object to iterate through.sort.
 	"""
 	def merge(arr, l, m, r):
 		n1 = m - l + 1
@@ -134,13 +164,17 @@ def mergeSort(iterable:list, zero:int, iterable_length_minus_one:int):
 				plt.pause(0.0001)
 				plt.clf()
 		return arr
-	if is_sorted(iterable):
-		return iterable
-	else:
-		return sort(iterable, zero, length_minus_one)
+	t = Timer()
+	while not is_sorted(iterable):
+		if is_sorted(iterable): break
+		else:
+			sort(iterable, 0, len(iterable)-1)
+	t.stop()
+	if timer or DEBUG: return t.return_result(iterable)
+	else: return iterable
 
 	
-def linearSearch(iterable:list, item):
+def linearSearch(iterable:list, item, timer=False):
 	"""
 	Checks if an item is in a list, by stepping through one item at a time and checking each item.
 	
@@ -153,12 +187,15 @@ def linearSearch(iterable:list, item):
 	The item to check for during the search.
 	"""
 	found = False
+	t = Timer()
 	for i in iterable:
 		if i == item: found = True; break
-	return found
+	t.stop()
+	if timer or DEBUG: return t.return_result(found)
+	else: found
 	
-	
-def binarySearch(iterable:list, item):
+
+def binarySearch(iterable:list, item, timer=False):
 	"""
 	Checks if an item is in a list, by halving the list recursively until it narrows down to two items and then checks if the desired item is one of the two items remaining.
 	
@@ -182,13 +219,17 @@ def binarySearch(iterable:list, item):
 		else:
 			if item in iterable: return True
 			else: return False
+	t = Timer()
 	if not is_sorted(iterable):
 		iterable = mergeSort(iterable, 0, len(iterable)-1)
 		if DEBUG: print("Sorted binary list")
-	return binary(iterable, item)
+	r = binary(iterable, item)
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
 
 	
-def largest(iterable:list):
+def largest(iterable:list, timer=False):
 	"""
 	Calculates the largest number in the list.
 	
@@ -198,12 +239,15 @@ def largest(iterable:list):
 	The list to iterate through.
 	"""
 	largest = 0
+	t = Timer()
 	for n in iterable:
 		if n > largest: largest = n
-	return largest
+	t.stop()
+	if timer or DEBUG: return t.return_result(largest)
+	else: return largest
 	
 	
-def smallest(iterable:list):
+def smallest(iterable:list, timer=False):
 	"""
 	Calculates the smallest number in the list.
 	
@@ -213,12 +257,15 @@ def smallest(iterable:list):
 	The list to iterate through.
 	"""
 	smallest = 0
+	t = Timer()
 	for n in iterable:
 		if n < smallest: smallest = n
-	return smallest
+	t.stop()
+	if timer or DEBUG: return t.return_result(smallest)
+	else: return smallest
 	
 	
-def evens(iterable:list):
+def evens(iterable:list, timer=False):
 	"""
 	Finds all the even numbers in a list.
 	
@@ -227,10 +274,14 @@ def evens(iterable:list):
 	iterable: `list`
 	The list to iterate through.
 	"""
-	return [n for n in iterable if n % 2 == 0]
+	t = Timer()
+	r = [n for n in iterable if n % 2 == 0]
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
 	
 	
-def odds(iterable:list):
+def odds(iterable:list, timer=False):
 	"""
 	Finds all the odd numbers in a list.
 	
@@ -239,10 +290,14 @@ def odds(iterable:list):
 	iterable: `list`
 	The list to iterate through.
 	"""
-	return [n for n in iterable if n % 2 != 0]
+	t = Timer()
+	r = [n for n in iterable if n % 2 != 0]
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
 	
 	
-def divisible(iterable:list, divider:int):
+def divisible(iterable:list, divider:int, timer=False):
 	"""
 	Finds all the numbers in a list that are divisible by the number passed.
 	
@@ -254,10 +309,14 @@ def divisible(iterable:list, divider:int):
 	divider: `int`
 	The number to check if divisible by.
 	"""
-	return [n for n in iterable if n % divider == 0]
+	t = Timer()
+	r = [n for n in iterable if n % divider == 0]
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
 	
 	
-def not_divisible(iterable:list, divider:int):
+def not_divisible(iterable:list, divider:int, timer=False):
 	"""
 	Finds all the numbers in a list that are not divisible by the number passed.
 	
@@ -269,9 +328,13 @@ def not_divisible(iterable:list, divider:int):
 	divider: `int`
 	The number to check if not divisible by.
 	"""
-	return [n for n in iterable if n % divider != 0]
+	t = Timer()
+	r = [n for n in iterable if n % divider != 0]
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
 
-def list2dict(iterable:list):
+def list2dict(iterable:list, timer=False):
 	"""
 	Converts a list to a dictionary.
 
@@ -282,9 +345,12 @@ def list2dict(iterable:list):
 	iterable: `list`
 		The list to convert.
 	"""
-	return {i[0]:i[1] for i in iterable}
+	t = Timer()
+	r = {i[0]:i[1] for i in iterable}
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
 
-def list2tuple(iterable:list):
+def list2tuple(iterable:list, timer=False):
 	"""
 	Converts a list to a tuple.
 
@@ -295,11 +361,18 @@ def list2tuple(iterable:list):
 	iterable: `list`
 		The list to convert.
 	"""
-	if type(iterable) != list: return iterable
-	return tuple(list2tuple(i) for i in iterable)
+	t = Timer()
+	if type(iterable) != list:
+		t.stop()
+		if timer or DEBUG: return iterable, t.result
+		else: return iterable
+	r = tuple(list2tuple(i) for i in iterable)
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
 
 
-def tuple2list(iterable:tuple):
+def tuple2list(iterable:tuple, timer=False):
 	"""
 	Converts a tuple to a list.
 
@@ -312,9 +385,18 @@ def tuple2list(iterable:tuple):
 	"""
 	if type(iterable) != tuple: return iterable
 	return list(tuple2list(i) for i in iterable)
+	t = Timer()
+	if type(iterable) != tuple:
+		t.stop()
+		if timer or DEBUG: return iterable, t.result
+		else: return iterable
+	r = list(tuple2list(i) for i in iterable)
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
 	
 
-def dict2list(dictionary:dict):
+def dict2list(dictionary:dict, timer=False):
 	"""
 	Converts a dictionary to a list.
 
@@ -325,4 +407,316 @@ def dict2list(dictionary:dict):
 	dictionary: `dict`
 		The dictionary to convert.
 	"""
-	return [[i,dictionary[i]] for i in [x for x in insertionSort([i[0] for i in list(dictionary.items())])]]
+	t = Timer()
+	r = [[i,dictionary[i]] for i in [x for x in insertionSort([i[0] for i in list(dictionary.items())])]]
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
+
+
+def reverse(iterable:list, timer=False):
+	"""
+	Reverses the order of items in a list.
+
+	# Attributes
+
+	iterable: `list`
+		The list of items to reverse.
+	"""
+	t = Timer()
+	r = [iterable[len(iterable)-i-1] for i in range(len(iterable))]
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
+
+
+def secs2mins(duration:float, decimals:int=0, timer=False):
+	"""
+	Converts a number from seconds to minutes by dividing by 60.
+
+	The result can be rounded to a certain number of decimal places (defaults to a whole number).
+
+	# Attributes
+
+	duration: `float`
+		The seconds to convert into minutes.
+
+	decimals: `int`
+		Number of decimal places to round the result to.
+	"""
+	t = Timer()
+	r = duration/60 if decimals < 0 else round(duration/60,decimals)
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
+
+
+def mins2hour(duration:float, decimals:int=0, timer=False):
+	"""
+	Converts a number from minutes to hours by dividing by 60.
+
+	The result can be rounded to a certain number of decimal places (defaults to a whole number).
+
+	# Attributes
+
+	duration: `float`
+		The seconds to convert into hours.
+
+	decimals: `int`
+		Number of decimal places to round the result to.
+	"""
+	t = Timer()
+	r = duration/60 if decimals < 0 else round(duration/60,decimals)
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
+
+
+def add(*nums, timer=False):
+	"""
+	Adds an unlimited amount of numbers together.
+
+	If you want to pass a list instead of a set of numbers separated by commas, then use `*` - for example `add(*nums)` where nums is a list of numbers.
+
+	For normal use, do `add(1,2)` which would return 3.
+
+	# Attributes
+	
+	*nums: `tuple`
+		The list of numbers as explained in the main description.
+	"""
+	total = 0
+	t = Timer()
+	for n in nums: total += n
+	t.stop()
+	if timer or DEBUG: return t.return_result(total)
+	else: return total
+
+
+def subtract(*nums, timer=False):
+	"""
+	Subtracts an unlimited amount of numbers together.
+
+	If you want to pass a list instead of a set of numbers separated by commas, then use `*` - for example `subtract(*nums)` where nums is a list of numbers.
+
+	For normal use, do `subtract(4,1)` which would return 3.
+
+	# Attributes
+	
+	*nums: `tuple`
+		The list of numbers as explained in the main description.
+	"""
+	total = 0
+	t = Timer()
+	for n in nums: total -= n
+	t.stop()
+	if timer or DEBUG: return t.return_result(total)
+	else: return total
+
+
+def multiply(*nums, timer=False):
+	"""
+	Multiplies an unlimited amount of numbers together.
+
+	If you want to pass a list instead of a set of numbers separated by commas, then use `*` - for example `multiply(*nums)` where nums is a list of numbers.
+
+	For normal use, do `multiply(3,2)` which would return 6.
+
+	# Attributes
+	
+	*nums: `tuple`
+		The list of numbers as explained in the main description.
+	"""
+	total = 1
+	t = Timer()
+	for n in nums: total *= n
+	t.stop()
+	if timer or DEBUG: return t.return_result(total)
+	else: return total
+
+
+def divide(*nums, timer=False):
+	"""
+	Divides an unlimited amount of numbers together.
+
+	If you want to pass a list instead of a set of numbers separated by commas, then use `*` - for example `divide(*nums)` where nums is a list of numbers.
+
+	For normal use, do `divide(3,2)` which would return 1.5.
+
+	# Attributes
+	
+	*nums: `tuple`
+		The list of numbers as explained in the main description.
+	"""
+	total = nums[0]
+	t = Timer()
+	if len(nums) > 1:
+		for n in nums[1:]: total /= n
+	t.stop()
+	if timer or DEBUG: return t.return_result(total)
+	else: return total
+
+
+def generate(start:int,end:int=None, timer=False):
+	"""
+	"""
+	if end is None: limit, start= start, 0
+	else: limit, start = end, start
+	t = Timer()
+	r = [n for n in range(start, limit)]
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
+
+
+def pi(decimal:int=9,timer=False):
+	k = 1
+	# Initialize sum
+	s = 0
+	for i in range(1000000):
+	    # even index elements are positive
+	    if i % 2 == 0:
+	        s += 4/k
+	    else:
+	        # odd index elements are negative
+	        s -= 4/k
+	    # denominator is odd
+	    k += 2
+	return s
+
+
+def factorial(value:int, timer=False):
+	"""
+	Calculates the factorial value of a number.
+
+	This is calculated with the formula: `n! = n x (n-1) x (n-2) ... 3 x 2 x 1`. `!` represents factorial.
+
+	# Attributes
+
+	value: `int`
+		The value to calculate the factorial of.
+	"""
+	r = value
+	t = Timer()
+	for i in reversed(list(range(1,value))): r *= i
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
+
+
+def sin(value:float, timer=False):
+	"""
+	Calculates the sine of a value.
+
+	# Attributes
+
+	value: `float`
+		The value to calculate the sine of.
+	"""
+	t = Timer()
+	r = math.sin(math.radians(value))
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
+
+
+def cos(value:float, timer=False):
+	"""
+	Calculates the cosine of a value.
+
+	# Attributes
+
+	value: `float`
+		The value to calculate the cosine of.
+	"""
+	t = Timer()
+	r = math.cos(math.radians(value))
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
+
+
+def tan(value:float, timer=False):
+	"""
+	Calculates the tangent of a value.
+
+	# Attributes
+
+	value: `float`
+		The value to calculate the tangent of.
+	"""
+	t = Timer()
+	r = math.tan(math.radians(value))
+	t.stop()
+	if timer or DEBUG: return t.return_result(r)
+	else: return r
+
+
+def mean(iterable:list, timer=False):
+	"""
+	Calculates the mean average of a list of items.
+
+	# Attributes
+
+	iterable: `list`
+		The list of items to find the average of.
+	"""
+	t = Timer()
+	total = 0
+	for i in iterable: total += i
+	total /= len(iterable)
+	t.stop()
+	if timer or DEBUG: return t.return_result(total)
+	else: return total
+
+
+def median(iterable:list, timer=False):
+	"""
+	Calculates the middle number of the list of items.
+
+	If the length of the list is even, it calculates the middle of the two middle numbers.
+
+	# Attributes
+
+	iterable: `list`
+		The list of items to find the middle of.
+	"""
+	t = Timer()
+	two = True if len(iterable)%2==0 else False
+	if two:
+		firstindex = int(len(iterable)/2)-1
+		secondindex = firstindex + 1
+		middle = iterable[secondindex]-((iterable[secondindex] - iterable[firstindex])/2)
+	else:
+		middle = iterable[len(iterable)//2]
+	t.stop()
+	if timer or DEBUG: return t.return_result(middle)
+	else: return middle
+
+
+def mode(iterable:list, timer=False):
+	"""
+	Calculates the most common number out of the list of items.
+
+	If there are two or more items that appear the same amount of time, then a `list` of those items is returned. Otherwise, the item is returned as its own data type.
+
+	# Attributes
+
+	iterable: `list`
+		The list of items to find the most common number from.
+	"""
+	t = Timer()
+	r = {i:0 for i in iterable}
+	for i in iterable: r[i] += 1
+	highest = []
+	for counted in r:
+		if len(highest) == 0: highest.append(counted)
+		else:
+			if r[counted] > r[highest[0]]:
+				highest = [counted]
+			elif r[counted] == r[highest[0]]:
+				highest.append(counted)
+	t.stop()
+	if len(highest) == 1: highest = highest[0]
+	if timer or DEBUG: return t.return_result(highest)
+	else: return highest
